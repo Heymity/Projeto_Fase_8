b8a799349255df65aaf951120e193e4e8a636a8c
Gabriel Pasquale <gabriel.prscavone@gmail.com>
2019-04-05 20:40:35 +0000

    Fazendo Despesas

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,14 +5,14 @@ class Api::V1::SessionsController < ApplicationController
         if user && user.valid_password?(sessions_params[:password])
             sign_in user, store: false
             user.generate_authentication_token!
-            user.save 
+            user.save
             render json: user, status:200
         else
-            render json: {errors: 'E-mail ou senha inválido' }, status:401
+            render json: {errors: 'E-mail ou senha inválido' }, status: 401
         end
     end
     
-    def destroy 
+    def destroy
         user = User.find_by(auth_token: params[:id])
         user.generate_authentication_token!
         user.save
@@ -21,8 +21,7 @@ class Api::V1::SessionsController < ApplicationController
     
     private
     
-    def session_params
+    def sessions_params
         params.require(:session).permit(:email, :password)
-    end
+    end    
 end
-            
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
     #devise_for :installs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     namespace :api, defaults: {format: :json} do
-        namespace :v1, path:"/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
-        
+        namespace :v1, path:"/", constraints: ApiVersionConstraint.new(version: 1, default: true) do       
             resources :users
+            resources :sessions
+            resources :gains
         end
     end
 end
